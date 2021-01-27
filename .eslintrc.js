1ef37a6fc34d53b59826f43cc8050143b2f36848
ozawaseijiro <seijiro-ozawa@iact.co.jp>
2021-01-27 18:56:19 +0900

    ci: prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,11 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: ["airbnb-base"],
+  extends: [
+    "airbnb-base",
+    "plugin:prettier/recommended",
+    "prettier/@typescript-eslint",
+  ],
   parser: "@typescript-eslint/parser",
   parserOptions: {
     ecmaVersion: 12,
